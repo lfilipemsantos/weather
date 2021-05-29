c3102a7ccab0951b465e3a3b8e37e43dc0d2197d
Luís Filipe Santos <luis.96.santos@hotmail.com>
2021-05-29 21:25:19 +0100

    New testing code
    - changes in api requests
    - added test plot of weather temperature
    - Design changes for mobile

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
-function get_data() {
-    var requestURL = "https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/1010500.json"
+/*function get_data() {
+    var requestURL = "https://api.ipma.pt/public-data/forecast/aggregate/1182100.json"
     var request = new XMLHttpRequest();
     request.open('GET', requestURL);
     request.responseType = 'json';
@@ -14,4 +14,47 @@ function get_data() {
             document.getElementById("min0").textContent = "Min: " + request.response['data'][0]["tMin"]
         }
     }
+}*/
+
+function get_data() {
+    var chart = new CanvasJS.Chart("chartContainer", {            
+        title:{
+            text: "Weekly Weather Forecast"              
+        },
+        axisY: {
+            suffix: " °C",
+            maximum: 40,
+            gridThickness: 0
+        },
+        toolTip:{
+            shared: true,
+            content: "{name} </br> <strong>Temperature: </strong> </br> Min: {y[0]} °C, Max: {y[1]} °C"
+        },
+        data: [{
+            type: "rangeSplineArea",
+            fillOpacity: 0.1,
+            color: "#91AAB1",
+            indexLabelFormatter: formatter,
+            dataPoints: [
+                { label: "Monday", y: [15, 26], name: "rainy" },
+                { label: "Tuesday", y: [15, 27], name: "rainy" },
+                { label: "Wednesday", y: [13, 27], name: "sunny" },
+                { label: "Thursday", y: [14, 27], name: "sunny" },
+                { label: "Friday", y: [15, 26], name: "cloudy" },
+                { label: "Saturday", y: [17, 26], name: "sunny" },
+                { label: "Sunday", y: [16, 27], name: "rainy" }
+            ]
+        }]
+    });
+    chart.render();
 }
+
+function formatter(e) { 
+	if(e.index === 0 && e.dataPoint.x === 0) {
+		return " Min " + e.dataPoint.y[e.index] + "°";
+	} else if(e.index == 1 && e.dataPoint.x === 0) {
+		return " Max " + e.dataPoint.y[e.index] + "°";
+	} else{
+		return e.dataPoint.y[e.index] + "°";
+	}
+} 
