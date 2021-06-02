8127f37504eba3cd8e7acf4858efc62674d489a8
Luís Filipe Santos <luis.96.santos@hotmail.com>
2021-06-02 18:43:02 +0100

    Design tweaks and API request
    - Design improvements
    - Started to implement requests to IPMA API

--- js.js
+++ js.js
@@ -58,3 +58,25 @@ function formatter(e) {
 		return e.dataPoint.y[e.index] + "°";
 	}
 } 
+
+function get_data() {
+    var requestURL = "https://api.ipma.pt/public-data/forecast/aggregate/1182100.json"
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+
+    request.onreadystatechange = function() {
+        if (request.readyState == XMLHttpRequest.DONE) {
+            console.log(request.response)
+            console.log(request.response['data'][0]);
+            console.log(request.response['data'][1]);
+            document.getElementById("max0").textContent = "Max: " + request.response['data'][0]["tMax"]
+            document.getElementById("min0").textContent = "Min: " + request.response['data'][0]["tMin"]
+        }
+        var hour;
+        for (hour = 0; hour < 24; hour++) {
+            console.log(request.response[hour]);
+        }
+    }
+}
