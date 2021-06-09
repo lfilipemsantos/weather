c3e46953f8157d2a9be39a010ee63aca1d1b3910
Luís Filipe Santos <luis.96.santos@hotmail.com>
2021-06-09 13:08:14 +0100

    Code refactoring

--- js.js
+++ js.js
@@ -10,131 +10,7 @@ if ('serviceWorker' in navigator) {
       });
   }
 
-function get_data() {
-    var requestURL = "https://api.ipma.pt/public-data/forecast/aggregate/1182100.json"
-    var request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    const current = new Date();
-    const time = current.toLocaleTimeString("pt-PT");
-
-    console.log(time.split(':', 1))
-    console.log(time);
-
-    request.onreadystatechange = function() {
-        if (request.readyState == XMLHttpRequest.DONE) {
-            var no_data = new Array ( 0 , 24 , 49 );
-            data = request.response
-
-            //fix response 
-            if(data[0]["tMed"]){
-                var temp = data[0];
-                data[0] = data[1];
-                data[1] = temp;
-            }
-
-            console.log(data);
-            build_table();
-
-
-            for (i=1; i<23; i++) {
-                var hour_split = data[i]["dataPrev"].split("T",2)[1].split(":")
-                var hour = hour_split[0] + ":" + hour_split[1]
-                console.log(hour.split(":", 1))
-                if (parseInt(hour.split(":"))<parseInt(time)) {
-                    continue;
-                }
-                
-                var row = document.createElement('div');
-                var temp = document.createElement('p');
-                var hour_text = document.createElement('p');
-                var weather_icon = document.createElement('img')
-                weather_icon.setAttribute("class", "weather_icon")
-                if (parseInt(hour.split(":"))<6 || parseInt(hour.split(":"))>21){
-                    weather_icon.src = "icons/n" + parseInt(data[i]["idTipoTempo"]) + ".svg"
-                }
-                else {
-                    weather_icon.src = "icons/d" + parseInt(data[i]["idTipoTempo"]) + ".svg"
-                }
-                temp.setAttribute('id', i);
-                temp.setAttribute('class', "hour_temp");
-                row.setAttribute('class', 'row');
-                temp.textContent = data[i]["tMed"] + "ºC";
-                hour_text.textContent = hour;
-                hour_text.setAttribute('class', "hour_time");
-                row.appendChild(temp);
-                row.appendChild(weather_icon);
-                row.appendChild(hour_text);
-                document.getElementById("innerToday").appendChild(row);
-                if (i+1 == time.split(':', 1)){
-                    document.getElementById("table_temp").textContent = data[i]["tMed"] + "ºC";
-                }
-            }
-
-            var hour_split = data[23]["dataPrev"].split("T",2)[1].split(":")
-            var hour = hour_split[0] + ":" + hour_split[1]
-            
-            var row = document.createElement('div');
-            var temp = document.createElement('p');
-            var hour_text = document.createElement('p');
-            var weather_icon = document.createElement('img')
-            weather_icon.setAttribute("class", "weather_icon")
-            if (parseInt(hour.split(":"))<6 || parseInt(hour.split(":"))>21){
-                weather_icon.src = "icons/n" + parseInt(data[i]["idTipoTempo"]) + ".svg"
-            }
-            else {
-                weather_icon.src = "icons/d" + parseInt(data[i]["idTipoTempo"]) + ".svg"
-            }
-            temp.setAttribute('id', "1"+23);
-            temp.setAttribute('class', "hour_temp");
-            row.setAttribute('class', 'row');
-            temp.textContent = data[23]["tMed"] + "ºC";
-            hour_text.textContent = hour;
-            hour_text.setAttribute('class', "hour_time");
-            row.appendChild(temp);
-            row.appendChild(weather_icon);
-            row.appendChild(hour_text);
-            document.getElementById("innerTomorrow").appendChild(row);
-            if (i+1 == time.split(':', 1)){
-                document.getElementById("table_temp").textContent = data[23]["tMed"] + "ºC";
-            }
-
-            for (i=25; i<48; i++) {
-                var hour_split = data[i]["dataPrev"].split("T",2)[1].split(":")
-                var hour = hour_split[0] + ":" + hour_split[1]
-                
-                var row = document.createElement('div');
-                var temp = document.createElement('p');
-                var hour_text = document.createElement('p');
-                var weather_icon = document.createElement('img')
-                if (parseInt(hour.split(":"))<6 || parseInt(hour.split(":"))>21){
-                    weather_icon.src = "icons/n" + parseInt(data[i]["idTipoTempo"]) + ".svg"
-                }
-                else {
-                    weather_icon.src = "icons/d" + parseInt(data[i]["idTipoTempo"]) + ".svg"
-                }
-                weather_icon.setAttribute("class", "weather_icon")
-                temp.setAttribute('id', "1"+i);
-                temp.setAttribute('class', "hour_temp");
-                row.setAttribute('class', 'row');
-                temp.textContent = data[i]["tMed"] + "ºC";
-                hour_text.textContent = hour;
-                hour_text.setAttribute('class', "hour_time");
-                row.appendChild(temp);
-                row.appendChild(weather_icon);
-                row.appendChild(hour_text);
-                document.getElementById("innerTomorrow").appendChild(row);
-                if (i+1 == time.split(':', 1)){
-                    document.getElementById("table_temp").textContent = data[i]["tMed"] + "ºC";
-                }
-            }
-        }
-    }
-}
-
 function switch_tab(id) {
-    console.log("id:" + id)
     var main = ["today", "tomorrow", "next_days"];
     for (let i = 0; i < main.length; i++) {
         document.getElementById(main[i]).style.display = "none";
@@ -144,23 +20,139 @@ function switch_tab(id) {
     document.getElementById(id+"_selector").classList.add("selected");
 }
 
-function build_table() {
-    document.getElementById("table_temp").textContent = data[0]["tMax"] + "ºC";
-    var update_date_string = "Última atualização: " + data[0]["dataUpdate"]
-    document.getElementById("data_update").textContent = update_date_string.replace("T", " às ")
+function get_data() {
+    //make request to api
+    var requestURL = "https://api.ipma.pt/public-data/forecast/aggregate/1182100.json"
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+
+    //get today and tomorrow date as string
+    const current = new Date();
+    var today_str = current.toISOString().slice(0,10);
+    current.setDate(current.getDate() +1);
+    var tomorrow_str = current.toISOString().slice(0,10);
+
+    request.onreadystatechange = function() {
+        if (request.readyState == XMLHttpRequest.DONE) {
+            var no_data = new Array ( 0 , 24 , 49 );
+            data = request.response;
+
+
+            var days = build_arrays(data, today_str, tomorrow_str)
+            console.log(data);
+            build_rows(days);
+        }
+    }
+}
+
+function build_table(current_day, current_hour) {
+
+    document.getElementById("table_temp").textContent = current_hour["tMed"] + "ºC";
+    document.getElementById("data_update").textContent = ("Última atualização: " + current_day["dataUpdate"]).replace("T", " às ")
+    
     var icon_name = ["tMax", "iUv", "tMin", "probabilidadePrecipita"];
+
     for (let i = 0; i < icon_name.length; i++) {
         var icon = document.createElement('img');
         icon.src="icons/icons_table/"+ icon_name[i] + ".svg";
         icon.classList.add("table_icons")
 
         var text = document.createElement('p');
-        if(icon_name[i] == "probabilidadePrecipita")
-            text.textContent = " " + data[0][icon_name[i]] + "%";
+        if(icon_name[i] == "probabilidadePrecipita") {
+            text.textContent = " " + current_day[icon_name[i]] + "%";
+        }
+        else if(icon_name[i].includes("tM")) {
+            text.textContent = " " + current_day[icon_name[i]] + "ºC";
+        }
         else {
-            text.textContent = " " + data[0][icon_name[i]];
+            text.textContent = " " + current_day[icon_name[i]];
         }
         text.prepend(icon);
-        document.getElementById(icon_name[i]).appendChild(text)
+        document.getElementById(icon_name[i]).appendChild(text);
     }
+}
+
+function build_rows(days) {
+    for(j=0;j<days.length;j++) {
+        if(j==0){
+            var row_id = "today";
+            var inner_id = "innerToday";
+        }
+        else if(j==1) {
+            var row_id = "tomorrow";
+            var inner_id = "innerTomorrow";
+        }
+        for(i=0;i<days[j].length;i++) {
+            var hour_split = days[j][i]["dataPrev"].split("T",2)[1].split(":");
+            var hour = hour_split[0] + ":" + hour_split[1];
+            
+            var row = document.createElement('div');
+            var temp = document.createElement('p');
+            var hour_text = document.createElement('p');
+            var weather_icon = document.createElement('img');
+            weather_icon.setAttribute("class", "weather_icon");
+            if (parseInt(hour.split(":"))<6 || parseInt(hour.split(":"))>21) {
+                weather_icon.src = "icons/n" + parseInt(days[j][i]["idTipoTempo"]) + ".svg";
+            }
+            else {
+                weather_icon.src = "icons/d" + parseInt(days[j][i]["idTipoTempo"]) + ".svg";
+            }
+            temp.setAttribute('id', i);
+            temp.setAttribute('class', "hour_temp");
+            row.setAttribute('class', 'row');
+            temp.textContent = days[j][i]["tMed"] + "ºC";
+            hour_text.textContent = hour;
+            hour_text.setAttribute('class', "hour_time");
+            row.appendChild(temp);
+            row.appendChild(weather_icon);
+            row.appendChild(hour_text);
+            document.getElementById(inner_id).appendChild(row);
+        }
+    }
+}
+
+function build_arrays(data, today_str, tomorrow_str) {
+    var today = new Array();
+    var tomorrow = new Array();
+
+    var current_day;
+    var current_hour;
+
+    const current = new Date();
+    const time = (current.toLocaleTimeString("pt-PT")).split(':',1);
+
+    for(i=0; i<data.length; i++) {
+        var hour_split = data[i]["dataPrev"].split("T",2)[1].split(":");
+        var hour = hour_split[0] + ":" + hour_split[1];
+
+        if(!(data[i]["tMed"]) && data[i]["dataPrev"].includes(today_str)) {
+            current_day = data[i];
+        }
+        else if(!(data[i]["tMed"])) {
+            continue;
+        }
+        else{
+            if(data[i]["dataPrev"].includes(today_str)) {
+
+                if (parseInt(hour.split(":"))<parseInt(time)) {
+                    continue;
+                }
+                else {
+                    today.push(data[i]);
+                    if (parseInt(hour.split(":"))==parseInt(time)) {
+                        current_hour = data[i];
+                    }
+                }
+            }
+            else if(data[i]["dataPrev"].includes(tomorrow_str)) {
+                tomorrow.push(data[i]);
+            }
+        }
+    }
+
+    build_table(current_day, current_hour)
+
+    return new Array(today, tomorrow);
 }
