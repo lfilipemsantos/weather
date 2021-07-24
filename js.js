7d09a10b407e5da773948bd4fbd0577df16edf09
Luís Filipe Santos <luis.96.santos@hotmail.com>
2021-07-25 00:40:08 +0100

    Design improvements
    - Added search button at the bottom options
    - Some design tweaks

--- js.js
+++ js.js
@@ -59,6 +59,7 @@ function build_locations(locations) {
 function switch_tab(id) {
     console.log("switching to " + id)
     var tabs = ["home-tab", "settings-tab", "location-tab", "favorites-tab"];
+    show_bottom();
     for (let i = 0; i < tabs.length; i++) {
         if(tabs[i]!=id){
             document.getElementById(tabs[i]).classList.add("out");
@@ -91,12 +92,18 @@ function switch_tab(id) {
         var fav_icon = document.getElementById("favorites_icon").src.replace("not_","is_");
         document.getElementById("favorites_icon").src = fav_icon;
     }
+    else if(id=="location-tab") {
+        document.getElementById("location").focus();
+        hide_bottom();
+    }
     
     setTimeout(() => {  document.getElementById(id).style.display = "block"; }, 400);
     
     if(id!="location-tab"){
         document.getElementById("bottom-options").style.display = "block";
     }
+
+
     return;
 }
 
@@ -405,6 +412,7 @@ function switch_theme(id) {
         document.getElementById("location").style.color = LIGHT;
         document.getElementById("location").style.opacity="90%";
         document.getElementById("close_icon").src = "close_w.svg";
+        document.getElementById("search_icon").src = "search_w.svg";
         document.getElementById("bottom-options").style.backgroundColor = "rgb(40, 40, 40)";
         document.getElementById("fav-notification-inner").style.backgroundColor = "rgb(40, 40, 40)";
         hr = document.getElementsByTagName("hr")
@@ -423,6 +431,8 @@ function switch_theme(id) {
         document.body.style.color=DARK;
         document.getElementById("location").style.color=DARK;
         document.getElementById("location").style.opacity="90%";
+        document.getElementById("close_icon").src = "close_b.svg";
+        document.getElementById("search_icon").src = "search_b.svg";
         document.getElementById("bottom-options").style.backgroundColor = "rgb(236, 236, 236)";
         document.getElementById("fav-notification-inner").style.backgroundColor = "rgb(206, 206, 206)";
         hr = document.getElementsByTagName("hr")
@@ -445,10 +455,10 @@ function switch_theme(id) {
 function switch_colors(color) {
     tables = document.getElementsByClassName("content_table");
     rows = document.getElementsByClassName("day_row");
-    change_color(rows,"backgroundColor",color, 0.2);
+    change_color(rows,"backgroundColor",color, 0.5);
     rows = document.getElementsByClassName("location-li");
-    change_color(rows,"backgroundColor",color, 0.2);
-    document.getElementById("location").style.backgroundColor = "rgba(" + color + ", 0.2)";
+    change_color(rows,"backgroundColor",color, 0.5);
+    document.getElementById("location").style.backgroundColor = "rgba(" + color + ", 0.5)";
     document.getElementById("apply-color-button").style.backgroundColor = "rgba(" + color + ", 0.1)";
     
     fav_rows = document.getElementsByClassName("fav_row");
@@ -508,7 +518,6 @@ function is_in_range(value) {
 function search_location() {
     switch_tab('location-tab');
     //hide_tab_buttons();
-    hide_bottom();
     var input, filter, ul, li, div, i, txtValue;
     input = document.getElementById("location");
     filter = input.value.toUpperCase();
@@ -526,10 +535,10 @@ function search_location() {
 }
 
 function hide_bottom()  {
-    document.getElementById("bottom-options").style.display = "none";
+    document.getElementById("bottom-options").style.bottom = "-80px";
 }
 function show_bottom()  {
-    document.getElementById("bottom-options").style.display = "block";
+    document.getElementById("bottom-options").style.bottom = "0px";
 }
 
 function add_favorite(id) {
